da7af07704cb919c996d9d0ae25a42bde53a605d
Ron Lara <lararon2428@gmail.com>
2022-09-26 16:45:53 +0800

    updated user features

--- src/api/user-game/controllers/user-game.js
+++ src/api/user-game/controllers/user-game.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * user-data controller
+ * user-game controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::user-data.user-data');
+module.exports = createCoreController('api::user-game.user-game');
